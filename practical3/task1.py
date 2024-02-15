c345c6f10619aad3e5280330c3d13e0e919ac356
IxedaUKD <160130419+IxedaUKD@users.noreply.github.com>
2024-02-15 13:58:59 +0200

    Add files via upload

---
+++ practical3/task1.py
@@ -0,0 +1,16 @@
+for i in range(1, 11):
+   print(i)
+print("----------")
+
+
+for i in range(1, 21):
+   if i % 2 == 0:
+       print(i)
+print("----------")
+
+
+for i in range(10, 0, -1):
+   print(i)
+print("----------")
+
+   
---
+++ practical3/task3.py
@@ -0,0 +1,20 @@
+class Person:
+   def __init__(self, name):
+       self.name = name
+
+
+   def greet(self):
+       return f"Привіт, мене звуть {self.name}"
+
+
+
+
+class Student(Person):
+   def is_student(self):
+       return True
+
+
+student = Student("Optimus Prime")
+print(student.greet())
+print(student.is_student())
+
